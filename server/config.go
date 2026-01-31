e1c38fc87884bf771177afdae12d279e8d3d3a92
Eka Anre Sangsya <ekaanresangsya@gmail.com>
2026-01-31 09:10:18 +0700

    adjust config viper

--- server/config.go
+++ server/config.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"log"
 	"os"
 	"strings"
 
@@ -17,15 +16,18 @@ func LoadConfig() *Config {
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	var config Config
 	if _, err := os.Stat(".env"); err == nil {
 		viper.SetConfigFile(".env")
 		_ = viper.ReadInConfig()
 	}
 
-	if err := viper.Unmarshal(&config); err != nil {
-		log.Fatalf("unable to load config: %v", err)
-	}
+	var config Config
+	// if err := viper.Unmarshal(&config); err != nil {
+	// 	log.Fatalf("unable to load config: %v", err)
+	// }
+
+	config.DBConn = viper.GetString("DB_CONN")
+	config.ServerPort = viper.GetString("SERVER_PORT")
 
 	return &config
 }
